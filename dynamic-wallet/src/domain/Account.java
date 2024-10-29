a3302a27cea71a5d02861781f5aa2d2af59e9a72
Cristian Diaz <diazcristian0210@gmail.com>
2024-10-28 22:57:27 -0300

    Agregamos el constructor de la clase account

--- dynamic-wallet/src/domain/Account.java
+++ dynamic-wallet/src/domain/Account.java
@@ -1,5 +1,6 @@
 package domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Account {
@@ -8,4 +9,13 @@ public class Account {
     protected double balance;
     protected List<Movement> movements; // lista de movimientos
     protected boolean enabled;
+
+    public Account(){
+        this.accountNumber = idGenerator; // se le asigna el ID único
+             this.balance = 0.0; // toda cuenta nueva comienza con balance en cero
+             this.movements = new ArrayList<>(); // toda cuenta tiene una lista vacía de movimientos
+             this.enabled = true; // cuenta habilitada por defecto
+     }
 }
+
+
