e914646454679f0a9ac2b767fdefa9b1d1e46c60
Cristian Diaz <diazcristian0210@gmail.com>
2024-10-28 22:50:17 -0300

    agregamos de manera protegida instancias como accountNumber, balance, movement y boolean enabled

--- dynamic-wallet/src/domain/Account.java
+++ dynamic-wallet/src/domain/Account.java
@@ -1,5 +1,11 @@
 package domain;
 
+import java.util.List;
+
 public class Account {
     private static int idGenerator = 0; // generador de IDs
+    protected int accountNumber;
+    protected double balance;
+    protected List<Movement> movements; // lista de movimientos
+    protected boolean enabled;
 }
