2d5bf7b4d22bd2b74a4e1891629a11f6d684e4c1
Héctor Armando Cortez <updown.input@gmail.com>
2024-11-05 10:18:08 -0300

    Clases Pesos y USD agregadas

---
+++ dynamic-wallet/src/domain/PesosAccount.java
@@ -0,0 +1,12 @@
+package domain;
+
+/*
+ * Cuenta hija que hereda de la clase Account todos sus
+ * atributos y métodos
+ * */
+public class PesosAccount extends Account {
+    // constructor clase hija
+    public PesosAccount() {
+        super(); // llamada al constructor de la clase madre (Account)
+    }
+}
