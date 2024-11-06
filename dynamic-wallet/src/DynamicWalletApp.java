98a3ad337e6672b6b10823537a0535a2adc81122
Héctor Armando Cortez <updown.input@gmail.com>
2024-11-06 10:43:05 -0300

    Método de inicio de base de datos y mensaje de bienvenida a la app

--- dynamic-wallet/src/DynamicWalletApp.java
+++ dynamic-wallet/src/DynamicWalletApp.java
@@ -1,5 +1,58 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.JOptionPane;
+
+import domain.Account;
+import domain.Customer;
+import domain.Movement;
+import domain.PesosAccount;
+import domain.USDAccount;
+
 public class DynamicWalletApp {
+    static List<Customer> customersDB;
+    static List<Account> accountsDB;
+
+    static Customer loggedUser;
+    static Movement movement;
+    static Account account;
+
     public static void main(String[] args) {
-        System.out.println("ejecución del proyecto en netbeans");
+        initDummyData();
+        welcomeMessage();
+    }
+
+    public static void initDummyData() {
+        customersDB = List.of(
+                new Customer("John", "Doe"),
+                new Customer("Alexa", "Smith"));
+
+        for (Customer customerSaved : customersDB) {
+            customerSaved.addAccount(new PesosAccount());
+            customerSaved.addAccount(new USDAccount());
+        }
+
+        accountsDB = new ArrayList<>();
+        for (Customer customerSaved : customersDB) {
+            accountsDB.addAll(customerSaved.getAccounts());
+        }
+    }
+
+    public static Account getAccountByAccountNumberFromDB(int accountNumber) {
+        for (Account accountSaved : accountsDB) {
+            if (accountSaved.getAccountNumber() == accountNumber) {
+                return accountSaved;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Muestra un mensaje de bienvenida al inicio de la ejecución
+     */
+    public static void welcomeMessage() {
+        String welcomeMessage = "Bienvenido a Dynamic Wallet\nSu billetera virtual dinámica" +
+                "\n\nFavor de registrarse para operar";
+        JOptionPane.showMessageDialog(null, welcomeMessage);
     }
 }
